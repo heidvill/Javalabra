7db86d692f161bfcc449514d7b46f3143c525ecf
unknown <heidi.villikka@helsinki.fi>
2013-03-12 23:25:43 +0200

    lisatty kayttoliittymaluokka

--- Tetris/src/tetris/Main.java
+++ Tetris/src/tetris/Main.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tetris;
 
-/**
- *
- * @author Asus
- */
 public class Main {
     
 }
---
+++ Tetris/src/tetris/gui/kayttoliittyma.java
@@ -0,0 +1,6 @@
+
+package tetris.gui;
+
+public class kayttoliittyma {
+    
+}
